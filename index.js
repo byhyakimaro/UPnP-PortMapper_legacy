95acbbf4b7b7fd7a1102fdfe9e167493519f30c9
byhyakimaro <moderrevolution090217@gmail.com>
2021-11-03 09:21:53 -0300

    add cli

--- index.js
+++ index.js
@@ -1,16 +1,60 @@
 const NatAPI = require('nat-api')
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+const prefix = '!'
 
 const client = new NatAPI({
   autoUpdate: true,
   enablePMP: true
 })
-const port = 3000
 
-client.unmap(port, function (err) {
-  if(!err) console.log('Port unmapped!') 
-})
+const commands = [
+  {
+    name: 'close',
+    run: function (port) {
+      client.unmap(port, function (err) {
+        if(!err) console.log(`Port ${port} closed with success!!`)
+        runCli()
+      })
+    }
+  },
+  {
+    name: 'open',
+    run: function (port) {
+      client.map({ publicPort: port, privatePort: port }, function (err) {
+        if (err) return console.log('Error', err)
+        console.log(`Port ${port} mapped with success!!`)
+        runCli()
+      })
+    }
+  }
+]
 
-client.map({ publicPort: port, privatePort: port }, function (err) {
-  if (err) return console.log('Error', err)
-  console.log(`Port ${port} mapped with success!!`)
-})
+async function runCli() { 
+  await new Promise(resolve => setTimeout(resolve, 100))
+  
+  readline.question("root@UPnP:~$ ", (cmd) => {
+    if (cmd.startsWith(prefix)) {
+      const cli = commands.find(({ name }) => name === cmd.split(' ')[0].split('!')[1].toLowerCase())
+      const port = cmd.split(' ')[1]
+      if(cli && port) {
+        cli.run(parseInt(port))
+      } 
+      else if (!port) {
+        console.log('No port specified')
+        runCli()
+      }
+      else {
+        console.log('command invalid')
+        runCli()
+      } 
+    } else {
+      console.log(`prefix is ${prefix}`)
+      runCli()
+    }
+  })
+}
+
+runCli()
